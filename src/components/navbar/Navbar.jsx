55fdbb66a14380a00b89b0b35e74e77c06da6b71
absaGH <absa3852@gmail.com>
2024-10-03 10:18:53 +0300

    Left and Right bar completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,10 +7,34 @@ import NotificationsOutlined from "@mui/icons-material/NotificationsOutlined";
 import EmailOutlined from "@mui/icons-material/EmailOutlined";
 import PersonOutlineOutlined from "@mui/icons-material/PersonOutlineOutlined";
 import SearchOutlined from "@mui/icons-material/SearchOutlined";
+import { Link } from "react-router-dom";
+import userImage from "/home/yosef/Desktop/react folders/React_Social_Media_Project/src/pages/images/image3.jpg";
 
 const Navbar = () => {
     return (
-        <div className='navBar'>Navbar</div>
+        <div className='navBar'>
+            <div className="left">
+              <Link to="/" style={{textDecoration:"none"}}>
+                <span>SimplexSocial</span>
+              </Link>
+              <HomeOutlinedIcon/>
+              <DarkModeOutlinedIcon/>
+              <GridViewOutlinedIcon/>
+              <div className="search">
+                <SearchOutlined/>
+                <input type="text" placeholder="Search..."/>
+              </div>
+            </div>
+            <div className="right">
+                <PersonOutlineOutlined/>
+                <EmailOutlined/>
+                <NotificationsOutlined/>
+                <div className="user">
+                    <img src={userImage} alt="" />
+                    <span>John Doe</span>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -1,8 +1,161 @@
 import "./rightBar.scss"
+import sampleIm from "/home/yosef/Desktop/react folders/React_Social_Media_Project/src/pages/images/image1.jpg";
 
 const RightBar = () => {
     return (
-        <div className='righttBar'>RightBar</div>
+        <div className="rightBar">
+            <span>Suggestions For You</span>
+            <div className="container">
+              <div className="item">
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={sampleIm} alt="" />
+                        <span>Jone Doe</span>
+                    </div>
+                    <div className="buttons">
+                        <button>follow</button>
+                        <button>dismiss</button>
+                    </div>
+                </div>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={sampleIm} alt="" />
+                        <span>Jone Doe</span>
+                    </div>
+                    <div className="buttons">
+                        <button>follow</button>
+                        <button>dismiss</button>
+                    </div>
+                </div>
+              </div>
+              <div className="item" >
+                <span>Latest Activities</span>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={sampleIm} alt="" />
+                        <p>
+                        <span>Jone Doe</span> changed their cover picture
+                        </p>
+                    </div>
+                    <span>1 min ago</span>
+                </div>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={sampleIm} alt="" />
+                        <p>
+                        <span>Jone Doe</span> changed their cover picture
+                        </p>
+                    </div>
+                    <span>1 min ago</span>
+                </div>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={sampleIm} alt="" />
+                        <p>
+                        <span>Jone Doe</span> changed their cover picture
+                        </p>
+                    </div>
+                    <span>1 min ago</span>
+                </div>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={sampleIm} alt="" />
+                        <p>
+                        <span>Jone Doe</span> changed their cover picture
+                        </p>
+                    </div>
+                    <span>1 min ago</span>
+                </div>
+              </div>
+              <div className="item">
+                <span>Online Friends</span>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={sampleIm} alt="" />
+                        <div className="online" />
+                        <span>Jone Doe</span>
+                    </div>
+                </div>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={sampleIm} alt="" />
+                        <div className="online" />
+                        <span>Jone Doe</span>
+                    </div>
+                </div>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={sampleIm} alt="" />
+                        <div className="online" />
+                        <span>Jone Doe</span>
+                    </div>
+                </div>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={sampleIm} alt="" />
+                        <div className="online" />
+                        <span>Jone Doe</span>
+                    </div>
+                </div>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={sampleIm} alt="" />
+                        <div className="online" />
+                        <span>Jone Doe</span>
+                    </div>
+                </div>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={sampleIm} alt="" />
+                        <div className="online" />
+                        <span>Jone Doe</span>
+                    </div>
+                </div>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={sampleIm} alt="" />
+                        <div className="online" />
+                        <span>Jone Doe</span>
+                    </div>
+                </div>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={sampleIm} alt="" />
+                        <div className="online" />
+                        <span>Jone Doe</span>
+                    </div>
+                </div>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={sampleIm} alt="" />
+                        <div className="online" />
+                        <span>Jone Doe</span>
+                    </div>
+                </div>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={sampleIm} alt="" />
+                        <div className="online" />
+                        <span>Jone Doe</span>
+                    </div>
+                </div>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={sampleIm} alt="" />
+                        <div className="online" />
+                        <span>Jone Doe</span>
+                    </div>
+                </div>
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={sampleIm} alt="" />
+                        <div className="online" />
+                        <span>Jone Doe</span>
+                    </div>
+                </div>
+              </div>
+            </div>
+        </div>
     )
 }
 
